3826321eb4dde4367ea847ec56090dff587a8b00
Sherazade B <sherazade.b86@gmail.com>
2018-05-21 14:07:56 +0200

    Add project files.

---
+++ ProjetArchi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjetArchi.Startup))]
+namespace ProjetArchi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
